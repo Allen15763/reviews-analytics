ded4414dbf39d3cb088852141490ddf9f5ae4641
Allen Li <love22798800@gmail.com>
2020-04-25 16:52:28 +0800

    calculate avg records

--- read.py
+++ read.py
@@ -8,9 +8,10 @@ with open('reviews.txt', 'r') as f:
 		if count % 1000 == 0:  # 求餘數 % 1000
 			print(len(data))
 		
-print(len(data))
+print('檔案讀取完了，總共有', len(data), '筆資料')
 
+sum_len = 0
+for avg in data:
+	sum_len += len(avg)
 
-print(data[0])
-print('-------------')
-print(data[1])
+print('每筆留言平均長度為', round(sum_len/len(data),0), '字')
